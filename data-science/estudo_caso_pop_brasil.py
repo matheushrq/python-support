983732c63e85233302c7d926d4adba2d5e6e702b
Matheus Henrique <henriquem320@gmail.com>
2025-10-07 21:57:06 -0300

    main: Estudo de caso da população brasileira entre 1980 - 2016 usando matplotlib.

---
+++ data-science/estudo_caso_pop_brasil.py
@@ -0,0 +1,21 @@
+# crescimento da população brasileira de 1980 a 2016
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dados = open('data-science/populacao_brasileira.csv').readlines()
+
+x = []
+y = []
+
+for i in range(len(dados)):
+    if i != 0:
+        linha = dados[i].strip().split(';')
+        x.append(int(linha[0]))
+        y.append(int(linha[1]))
+
+plt.bar(x, y, color='gray', alpha=0.3)
+plt.plot(x, y, color='black', marker='_')
+plt.title("Crescimento da População Brasileira (1980-2016)")
+plt.xlabel("Ano")
+plt.ylabel("População (x100.000.000)")
+plt.show()
