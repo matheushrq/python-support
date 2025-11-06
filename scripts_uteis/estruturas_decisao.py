dfcdc202e58c6407ef33534d0b877b9985762e85
Matheus Henrique <henriquem320@gmail.com>
2025-11-06 16:50:56 -0300

    main: Prática com estruturas de decisão.

---
+++ scripts_uteis/estruturas_decisao.py
@@ -0,0 +1,21 @@
+nota = 7
+if (nota >= 7):
+    print("Aprovado!")
+else:
+    print("Reprovado!")
+
+# mudando o valor da variável nota
+nota = 5
+if (nota >= 7):
+    print("Aprovado!")
+else:
+    print("Reprovado!")
+
+# testar com 4, 5 e 7
+nota = 7
+if (nota <= 4):
+    print("Reprovado!")
+elif (nota > 5 and nota < 7):
+    print("Recuperação!")
+else:
+    print("Aprovado!")
